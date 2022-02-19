ed62ccc5d504ac18f95edf0673f8c1a15079e273
palreddysai <saikumar.palreddy@gmail.com>
2022-02-19 11:58:20 -0600

    new change

--- src/app/git-repos/git-repos.component.ts
+++ src/app/git-repos/git-repos.component.ts
@@ -7,36 +7,36 @@ import { GitrepoService } from '../shared/gitrepo.service';
   styleUrls: ['./git-repos.component.css']
 })
 export class GitReposComponent implements OnInit {
-  userName:string = "johnpapa";
-  getProfileResp : any = [];
+  userName: string = "johnpapa";
+  getProfileResp: any = [];
   getRepos: any;
-  selected_page:any;
+  selected_page: any;
 
   currentPage = 1;
   contentLoaded: boolean = false;
-  constructor(private _service: GitrepoService) {}
+  constructor(private _service: GitrepoService) { }
 
 
   ngOnInit(): void {
-    setInterval(()=>{
-      this.contentLoaded = true
-      this.getProfile();
+    // setInterval(()=>{
+    //   this.contentLoaded = true
+    //   this.getProfile();
 
-    },4000)
+    // },4000)
 
   }
 
 
-  getProfile (){
-    this._service.getUser(this.userName).subscribe( getUserResponse => {
+  getProfile() {
+    this._service.getUser(this.userName).subscribe(getUserResponse => {
       this.getProfileResp = getUserResponse;
       this.contentLoaded = true;
       this.getRepositories();
     })
   }
-  getRepositories (){
-    this._service.getRepos(this.userName).subscribe( getReposResponse => {
-      console.log("entering",this.selected_page)
+  getRepositories() {
+    this._service.getRepos(this.userName).subscribe(getReposResponse => {
+      console.log("entering", this.selected_page)
       this.contentLoaded = true
       this.getRepos = getReposResponse;
     })
--- src/app/shared/gitrepo.service.ts
+++ src/app/shared/gitrepo.service.ts
@@ -9,19 +9,20 @@ import { HttpClient } from '@angular/common/http';
 export class GitrepoService {
 
   clientId = '1e8cddce2e4bc23f234b';
-  clientSecret ='b0fce61a100732ccccc781bbad25321fe5bea185';
+  clientSecret = 'b0fce61a100732ccccc781bbad25321fe5bea185';
 
-page:number = 1;
- item_per_page:number = 100;
-  constructor(private http:HttpClient) { }
+  page: number = 1;
+  item_per_page: number = 100;
+  constructor(private http: HttpClient) { }
 
-  getUser(userName:string){
+  getUser(userName: string) {
     return this.http.get('https://api.github.com/users/' + userName + '?client_id='
-  + this.clientId + '&client_secret=' + this.clientSecret).pipe(map(res => res));
+      + this.clientId + '&client_secret=' + this.clientSecret).pipe(map(res => res));
   }
 
-  getRepos(userName:string){
+  getRepos(userName: string) {
     return this.http.get('https://api.github.com/users/' + userName + '/repos?client_id='
-  + this.clientId + '&client_secret=' + this.clientSecret + '?page=' + this.page +'&per_page=' + this.item_per_page + '\&page=1').pipe(map(res => res));
+      + this.clientId + '&client_secret=' + this.clientSecret + '?page=' + this.page + '&per_page=' + this.item_per_page + '\&page=1;rel="next"').pipe(map(res => res));
   }
+  
 }
